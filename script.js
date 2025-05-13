1c5395ed6c89883612dc4a297dc841622b84ad78
simog <192517110+SimoG3@users.noreply.github.com>
2025-05-13 15:10:44 +0100

    added music bg and button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const gameButtons = document.querySelectorAll('.gameStart');
     const game = document.getElementById("gameBoard");
+    const music = document.getElementById("bg-music");
+    const toggleMusic = document.getElementById("toggleMusic");
+    const icon = toggleMusic.querySelector("i");
   
     gameButtons.forEach(button => {
       button.addEventListener("click", () => {
@@ -9,12 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
         //hide game mode selection
         document.getElementById("gameSelection").style.display = 'none';
+
+        //start music
+        music.play();
   
         // Show game: remove hidden and fade in
         game.classList.remove("hidden");
         void game.offsetWidth; // force reflow
         game.classList.add("show");
       });
+    });
+
+    //music toggle
+    toggleMusic.addEventListener("click", () => {
+      if (music.paused) {
+        music.play();
+        icon.classList.remove("fa-play");
+        icon.classList.add("fa-pause");
+      } else {
+        music.pause();
+        icon.classList.remove("fa-pause")
+        icon.classList.add("fa-play");
+      }
     });
   });
   
